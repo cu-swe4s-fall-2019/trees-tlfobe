828bd94ada3b7b53408b15b6121f2b5ce945e184
tlfobe <lennyfobe@gmail.com>
2019-11-07 00:03:59 -0700

    added files for insert_key_value_pairs.py as well as functional test

---
+++ insert_key_value_pairs.py
@@ -0,0 +1,11 @@
+import binary_tree
+import sys
+import argparse
+sys.path.insert(1, "hash-tables-tlfobe")  # noqa: E402
+sys.path.insert(1, "avl_tree")  # noqa: E402
+import hash_tables
+import avl
+
+
+def main():
+    pass
